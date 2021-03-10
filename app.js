b8e5740970671d679e6b25241fa8a68a6e679e81
Raymond Lee <raymanl33@icloud.com>
2021-03-09 16:23:40 -0800

    Adjust readable mobile layout and JS add

---
+++ app.js
@@ -0,0 +1,14 @@
+let num1 = 7
+let num2 = 10
+const intro = "Hello my name is Raymond"
+Boolean (123654 > 12)
+
+if (num1 === num2) {
+    console.log("they match")
+  } else {
+    console.log("no luck")
+  }
+
+  for (character of intro) {
+    console.log(character)
+  }
